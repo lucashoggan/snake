5436adae948b55043745e506743e89f6a1eb6831
Lucas Hoggan <lucashoggan@yahoo.com>
2025-05-31 21:26:41 +0100

    v1.0.0

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <SDL3/SDL.h>
 #include <iostream>
 #include <algorithm>
+#include <random>
 
 const int WINDOW_WIDTH = 800;
 const int WINDOW_HEIGHT = 600;
@@ -35,16 +36,14 @@ void renderBoard(SDL_Renderer *renderer, int board[BOARD_COLS*BOARD_ROWS]) {
         }
     }
 }
-/*
-void devPrintBoard(int board[BOARD_COLS*BOARD_ROWS]) {
-    for (int i=0;i<BOARD_ROWS;i++) {
-        for (int j=0;i<BOARD_COLS;j++) {
-            std::cout << board[BOARD_ROWS*i+j] << " ";
-        }
-        std::cout << std::endl;
-    }
+
+int random_int(int min, int max) {
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> distrib(min, max);
+    return distrib(gen);
 }
-*/
+
 void getSnakeHeadPos(int board[BOARD_ROWS*BOARD_COLS], int snakeLength, int &r, int &c) {
     for (int i=0;i<BOARD_ROWS;i++) {
         for (int j=0;j<BOARD_COLS;j++) {
@@ -77,15 +76,52 @@ bool moveHitSnake(int board[BOARD_ROWS*BOARD_COLS], int snakeLength, int dir) {
     int snakeHeadPos[2] = {-1, -1}; // row n, col n
     getSnakeHeadPos(board, snakeLength, snakeHeadPos[0], snakeHeadPos[1]);
     if (dir == 0) /* upwards */ {
-        if (board[BOARD_ROWS*(snakeHeadPos[0]-1)+snakeHeadPos[1]] < 0) return true;
+        if (board[BOARD_ROWS*(snakeHeadPos[0]-1)+snakeHeadPos[1]] > 0) return true;
     } else if (dir == 1) /* right */ {
-        if (board[BOARD_ROWS*snakeHeadPos[0] + snakeHeadPos[1]+1] < 0) return true;
+        if (board[BOARD_ROWS*snakeHeadPos[0] + snakeHeadPos[1]+1] > 0) return true;
     } else if (dir == 2) /* down */ {
-        if (board[BOARD_ROWS*(snakeHeadPos[0]+1)+snakeHeadPos[1]] < 0) return true;
+        if (board[BOARD_ROWS*(snakeHeadPos[0]+1)+snakeHeadPos[1]] > 0) return true;
     } else if (dir == 3) /* left */ {
-        if (board[BOARD_ROWS*snakeHeadPos[1] + snakeHeadPos[1]-1] < 0) return true;
+        if (board[BOARD_ROWS*snakeHeadPos[1] + snakeHeadPos[1]-1] > 0) return true;
     }
     return false;
+}
+
+bool moveHitApple(int board[BOARD_ROWS*BOARD_COLS], int snakeLength, int dir) {
+    int snakeHeadPos[2] = {-1, -1}; // row n, col n
+    getSnakeHeadPos(board, snakeLength, snakeHeadPos[0], snakeHeadPos[1]);
+    if (dir == 0) /* upwards */ {
+        if (board[BOARD_ROWS*(snakeHeadPos[0]-1)+snakeHeadPos[1]] == -1) return true;
+    } else if (dir == 1) /* right */ {
+        if (board[BOARD_ROWS*snakeHeadPos[0] + snakeHeadPos[1]+1] == -1) return true;
+    } else if (dir == 2) /* down */ {
+        if (board[BOARD_ROWS*(snakeHeadPos[0]+1)+snakeHeadPos[1]] == -1) return true;
+    } else if (dir == 3) /* left */ {
+        if (board[BOARD_ROWS*snakeHeadPos[1] + snakeHeadPos[1]-1] == -1) return true;
+    }
+    return false;
+}
+
+void generateApple(int board[BOARD_ROWS*BOARD_COLS]) {
+    int idx;
+    do {
+        idx = random_int(0, BOARD_ROWS*BOARD_COLS);
+    } while (board[idx] != 0);
+    board[idx] = -1;
+}
+
+void addToSnake(int board[BOARD_ROWS*BOARD_COLS], int snakeLength, int dir) {
+    int snakeHeadPos[2] = {-1, -1}; // row n, col n
+    getSnakeHeadPos(board, snakeLength, snakeHeadPos[0], snakeHeadPos[1]);
+    if (dir == 0) /* upwards */ {
+        board[BOARD_ROWS*(snakeHeadPos[0]-1)+snakeHeadPos[1]] = snakeLength+1;
+    } else if (dir == 1) /* right */ {
+        board[BOARD_ROWS*snakeHeadPos[0]+snakeHeadPos[1]+1] = snakeLength+1;
+    } else if (dir == 2) /* downwards */ {
+        board[BOARD_ROWS*(snakeHeadPos[0]+1)+snakeHeadPos[1]] = snakeLength+1;
+    } else if (dir == 3) /* left */ {
+        board[BOARD_ROWS*snakeHeadPos[0]+snakeHeadPos[1]-1] = snakeLength+1;
+    }
 }
 
 void moveSnake(int board[BOARD_ROWS*BOARD_COLS], int snakeLength, int dir /* direction */) {
@@ -118,6 +154,7 @@ int main() {
     board[0] = 1;
     board[1] = 2;
     board[2] = 3;
+    generateApple(board);
     
     //devPrintBoard(board);
 
@@ -143,8 +180,21 @@ int main() {
             if (event.type == SDL_EVENT_QUIT) {
                 running = false;
             }
-        }
 
+            if (event.type == SDL_EVENT_KEY_DOWN) {
+                if (event.key.key == SDLK_UP && dir != 2) {
+                    dir = 0;
+                } else if (event.key.key == SDLK_RIGHT && dir != 3) {
+                    dir = 1;
+                } else if (event.key.key == SDLK_DOWN && dir != 0) {
+                    dir = 2;
+                } else if (event.key.key = SDLK_LEFT && dir != 1) {
+                    dir = 3;
+                }
+
+            }
+        }
+        /*
         keys = SDL_GetKeyboardState(nullptr);
         if (keys[SDL_SCANCODE_UP]) {
             dir = 0;
@@ -155,6 +205,7 @@ int main() {
         } else if (keys[SDL_SCANCODE_LEFT]) {
             dir = 3;
         }
+            */
 
         if (!moveInBound(board, snakeLength, dir)) {
             running = false;
@@ -172,13 +223,17 @@ int main() {
             SDL_DestroyWindow(window);
             SDL_Quit();
             return 0;
+        } else if (moveHitApple(board, snakeLength, dir)) {
+            addToSnake(board, snakeLength, dir);
+            snakeLength++;
+            generateApple(board);
         } else {
 
             moveSnake(board, snakeLength, dir);
 
             renderBoard(renderer, board);
             SDL_RenderPresent(renderer);
-            SDL_Delay(75);
+            SDL_Delay(120);
         }
     }
 
