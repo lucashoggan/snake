da8ff78b2f0fabd44624c8cd99db2c59d50aac58
Lucas Hoggan <lucashoggan@yahoo.com>
2025-05-31 21:54:32 +0100

    v1.1.0 - added infinite play and multi apple

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,14 @@
 #include <algorithm>
 #include <random>
 
+const bool INFINITE_PLAY = true;
+const int APPLE_COUNT = 4;
+
+
 const int WINDOW_WIDTH = 800;
 const int WINDOW_HEIGHT = 600;
+
+
 
 const int BOARD_ROWS = 20;
 const int BOARD_COLS = 20;
@@ -110,6 +116,14 @@ void generateApple(int board[BOARD_ROWS*BOARD_COLS]) {
     board[idx] = -1;
 }
 
+bool minApplesMet(int board[BOARD_ROWS*BOARD_COLS]) {
+    int count = 0;
+    for (int i;i<BOARD_ROWS*BOARD_COLS;i++) {
+        if (board[i] == -1) count+=1;
+    }
+    return APPLE_COUNT == count;
+}
+
 void addToSnake(int board[BOARD_ROWS*BOARD_COLS], int snakeLength, int dir) {
     int snakeHeadPos[2] = {-1, -1}; // row n, col n
     getSnakeHeadPos(board, snakeLength, snakeHeadPos[0], snakeHeadPos[1]);
@@ -154,7 +168,7 @@ int main() {
     board[0] = 1;
     board[1] = 2;
     board[2] = 3;
-    generateApple(board);
+    for (int i=0;i<APPLE_COUNT;i++) (generateApple(board));
     
     //devPrintBoard(board);
 
@@ -191,6 +205,7 @@ int main() {
                 } else if (event.key.key = SDLK_LEFT && dir != 1) {
                     dir = 3;
                 }
+                
 
             }
         }
@@ -206,23 +221,37 @@ int main() {
             dir = 3;
         }
             */
-
+        if (!minApplesMet(board)){
+            generateApple(board);
+        }
         if (!moveInBound(board, snakeLength, dir)) {
-            running = false;
             std::fill_n(board, BOARD_ROWS*BOARD_COLS, -1);
             renderBoard(renderer, board);
             SDL_RenderPresent(renderer);
-            SDL_DestroyWindow(window);
-            SDL_Quit();
-            return 0;
+            if (!INFINITE_PLAY) running = false;
+            else {
+                snakeLength = 3;
+                dir = 1; // direction
+                std::fill_n(board, BOARD_ROWS*BOARD_COLS, 0);
+                board[0] = 1;
+                board[1] = 2;
+                board[2] = 3;
+                for (int i=0;i<APPLE_COUNT;i++) (generateApple(board));
+            }
         } else if (moveHitSnake(board, snakeLength, dir)) {
-            running = false;
             std::fill_n(board, BOARD_ROWS*BOARD_COLS, -1);
             renderBoard(renderer, board);
             SDL_RenderPresent(renderer);
-            SDL_DestroyWindow(window);
-            SDL_Quit();
-            return 0;
+            if (!INFINITE_PLAY) running = false;
+            else {
+                snakeLength = 3;
+                dir = 1; // direction
+                std::fill_n(board, BOARD_ROWS*BOARD_COLS, 0);
+                board[0] = 1;
+                board[1] = 2;
+                board[2] = 3;
+                for (int i=0;i<APPLE_COUNT;i++) (generateApple(board));
+            }
         } else if (moveHitApple(board, snakeLength, dir)) {
             addToSnake(board, snakeLength, dir);
             snakeLength++;
@@ -233,8 +262,11 @@ int main() {
 
             renderBoard(renderer, board);
             SDL_RenderPresent(renderer);
-            SDL_Delay(120);
+            
         }
+        SDL_Delay(120);
+        
+        
     }
 
     SDL_DestroyWindow(window);
